ea9e28bc4af58e1c6289497c7f7114aec5cb98c7
r-strilets <55804526+r-strilets@users.noreply.github.com>
2022-09-08 18:52:50 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,16 @@
 import Notiflix from 'notiflix';
 export function fetchCountries(name) {
+  if (!name) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    return;
+  }
   return fetch(
     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
-      if (!response.ok) {
-        throw new Error('Oops, there is no country with that name');
-      }
       return response.json();
     })
     .then(data => {
-      // console.log(data);
       if (data.length > 10)
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
